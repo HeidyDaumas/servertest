f46d973854783649c0052c8aeb0ba42c1e6697c6
HeidyDaumas <96191482+HeidyDaumas@users.noreply.github.com>
2025-01-15 15:27:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,23 +6,21 @@ const server = new WebSocket.Server({ port: PORT });
 server.on("connection", (socket) => {
   console.log("Client connected");
 
+  // Send "Hello World" to the client upon connection
+  socket.send("Hello World");
+
   socket.on("message", (message) => {
     console.log(`Received: ${message}`);
+    
+    // Log the message type
     if (typeof message === "string") {
       console.log(`Received string: ${message}`);
-      server.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(message);
-        }
-      });
     } else {
       console.log("Received Uint8Array data");
-      server.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(message, { binary: true });
-        }
-      });
     }
+
+    // Respond with "Hello World" for every received message
+    socket.send("Hello World");
   });
 
   socket.on("close", () => {
