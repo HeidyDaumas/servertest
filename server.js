e059ca8c9b541cfaf9873af90b5f066fd0b6d74b
HeidyDaumas <96191482+HeidyDaumas@users.noreply.github.com>
2025-01-15 18:04:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 const WebSocket = require("ws");
-
 const PORT = process.env.PORT || 8080;
 const server = new WebSocket.Server({ port: PORT });
 
 server.on("connection", (socket) => {
   console.log("Client connected");
-
   // Send "Hello World" to the client upon connection
   socket.send("Hello World");
-
+  
   socket.on("message", (message) => {
-    console.log(`Received: ${message}`);
-    
-    // Log the message type
-    if (typeof message === "string") {
-      console.log(`Received string: ${message}`);
-    } else {
-      console.log("Received Uint8Array data");
-    }
-
-    // Respond with "Hello World" for every received message
-    socket.send("Hello World");
+    // Just receive the data without logging anything
   });
 
   socket.on("close", () => {
